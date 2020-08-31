a719ab92fd0960d3ab4cd8c9971a77f5446351cb
mariaYunB <maria.yun.b@gmail.com>
2020-08-31 12:26:26 +0300

    position calculation replaced with observer

--- js/effect_for_icons.js
+++ js/effect_for_icons.js
@@ -1,38 +1,22 @@
 const icons = document.querySelectorAll(`.privileges__icon`);
 
-icons.forEach(icon => {
-  icon.style.filter = `opacity(0.2)`;
-  icon.style.height = 50 + `px`;
-})
-
-function isElementVisible(el) {
-  const rect   = el.getBoundingClientRect(),
-      vWidth   = window.innerWidth || document.documentElement.clientWidth,
-      vHeight  = window.innerHeight || document.documentElement.clientHeight,
-      efp      = (x, y) => document.elementFromPoint(x, y);
-
-  // Return false if el isn`t in the viewport
-  if (rect.right < 0 || rect.bottom < 0 || rect.left > vWidth || rect.top > vHeight)
-    return false;
-
-  // Return true if any of el`s four corners are visible
-  return (el.contains(efp(rect.left,  rect.top))
-    ||  el.contains(efp(rect.right, rect.top))
-    ||  el.contains(efp(rect.right, rect.bottom))
-    ||  el.contains(efp(rect.left,  rect.bottom))
-  );
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1
 }
 
-window.addEventListener(`scroll`, () => {
-  icons.forEach(icon => {
-    setTimeout(() => {
-      if (isElementVisible(icon)) {
-        icon.style.filter = null;
-        icon.style.height = null;
-      } else {
-        icon.style.filter = `opacity(0.2)`;
-        icon.style.height = 50 + `px`;
-      }
-    }, 1000);
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const targetIcon = entry.target;
+      targetIcon.style.opacity = 1;
+      observer.unobserve(targetIcon);
+    }
   })
-});
+}, options)
+
+icons.forEach(icon => {
+    icon.style.opacity = 0;
+    observer.observe(icon);
+})
